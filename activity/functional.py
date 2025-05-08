d53a6f323616ef5eba352ae555d413239533c5f0
Stella Sun <stellasun0332@gmail.com>
2025-05-08 12:51:25 -0500

    Completed wave 01&02

--- activity/functional.py
+++ activity/functional.py
@@ -2,15 +2,17 @@
 # Write a function that takes in a list of words and returns the shortest
 # one. (assume no ties)
 
+
 def shortest_word(words):
-    pass
+    return min(words, key=len)
+
 
 # SUPER CHALLENGE: use functools.reduce
 # Only try this after completing the rest of the activity
 # You will need to research the use of the functools.reduce function
 # def shortest_word(words):
 #     from functools import reduce
-#     pass    
+#     pass
 
 
 # Wave 2
@@ -18,30 +20,36 @@ def shortest_word(words):
 # containing only the ones that are even, in the same order.
 # Hint: remember to convert back to a list!
 
+
 def even_nums(nums):
-    pass
+    return list(filter(lambda x: x % 2 == 0, nums))
+
 
 # Wave 3
 # Write a function that takes in a list of numbers and returns a new list
 # containing the squares of those numbers (the numbers raised to the second power)
 # Hint: remember to convert back to a list!
 
+
 def squares(nums):
     pass
 
+
 # Wave 4
-# Write a function that accepts a word, a function, and the name of that 
+# Write a function that accepts a word, a function, and the name of that
 # function. It should return a string that reports:
 # "The result of applying FUNCTION_NAME to WORD is RESULT"
+
 
 def report(word, function, function_name):
     pass
 
+
 # Wave 5
-# Write a function that takes a list of passwords and returns a list of only 
-# those passwords that have at least one non-alphabetic character in them. 
+# Write a function that takes a list of passwords and returns a list of only
+# those passwords that have at least one non-alphabetic character in them.
 # The returned list should be sorted by in order of increasing length.
+
 
 def sorted_valid_passwords(passwords):
     pass
-
